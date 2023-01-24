1123cbfea937a6ee3fb280013db105f215f725bd
John Lindquist <johnlindquist@gmail.com>
2023-01-24 12:47:33 -0700

    fix(windows): testing windows node

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 #! /usr/bin/env node
 
 import kit from "@johnlindquist/kit"
+import { createPathResolver } from "@johnlindquist/kit/core/utils"
 
 import * as path from "path"
 const currentScriptUrl = import.meta.url
 const currentScriptPath = path.dirname(currentScriptUrl.replace("file://", ""))
-process.chdir(currentScriptPath)
 
-await kit("download-node")
+let currentDir = createPathResolver(currentScriptPath)
+
+await kit(currentDir("download-node.js"))
