92041b0c300122907585556dfd5d84489e347d89
John Lindquist <johnlindquist@gmail.com>
2023-01-24 12:07:50 -0700

    fix(windows): testing windows node

--- index.js
+++ index.js
@@ -2,4 +2,9 @@
 
 import kit from "@johnlindquist/kit"
 
+import * as path from "path"
+const currentScriptUrl = import.meta.url
+const currentScriptPath = path.dirname(currentScriptUrl.replace("file://", ""))
+process.chdir(currentScriptPath)
+
 await kit("download-node")
