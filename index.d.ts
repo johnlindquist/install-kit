6d645cd5cd008adbe4311825840125fe50850b18
John Lindquist <johnlindquist@gmail.com>
2023-01-26 11:17:36 -0700

    fix(types): adding a types file

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+export let downloadNode: () => Promise<void>
+export let downloadKit: () => Promise<void>
+export let downloadKenv: () => Promise<void>
+export let installEsbuild: () => Promise<void>
+export let setup: () => Promise<void>
