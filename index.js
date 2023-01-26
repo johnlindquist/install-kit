a65ba537d946bddc64c224c70725d9bb67375bd1
John Lindquist <johnlindquist@gmail.com>
2023-01-26 13:09:25 -0700

    fix(index): switched node/kit

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ console.log(`Setting up Kit SDK for Kit app version: ${process.env.KIT_APP_VERSI
 let scriptParentPath = createPathResolver(path.dirname(fileURLToPath(new URL(import.meta.url))))
 
 export let downloadNode = async () => {
-  await kit(scriptParentPath("download-kit.js"))
+  await kit(scriptParentPath("download-node.js"))
 }
 
 export let downloadKit = async () => {
-  await kit(scriptParentPath("download-node.js"))
+  await kit(scriptParentPath("download-kit.js"))
 }
 
 export let downloadKenv = async () => {
